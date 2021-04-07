53c3768a603032adc5d5f339e195462fb90589b7
thakurdeepika1493 <74904991+thakurdeepika1493@users.noreply.github.com>
2021-04-07 17:42:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+var database;
+var position;
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+var balloonpositionref=database.ref("balloon/position");
+balloonpositionref.on("value",readPosition,showError);
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updatePosition(-10,0);
+    //write code to move air balloon in left direction
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updatePosition(10,0);
+    //write code to move air balloon in right direction
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updatePosition(-10,0);
+    balloon.scale+=0.005;
+    //write code to move air balloon in up direction
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    updatePosition(10,0);
+    balloon.scale-=0.005;
+    //write code to move air balloon in down direction
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+function readPosition(data){
+position=data.val();
+balloon.x=position.x;
+balloon.y=position.y;
+}
+function showError(){
+console.log("there was an error in the database");
+
+}
+
+function updatePosition(x,y){
+database.ref("balloon/position").set({
+  "x":balloon.x+x,"y":balloon.y+y
+})
+
+
+}
+
+
